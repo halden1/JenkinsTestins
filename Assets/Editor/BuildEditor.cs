64f39201c2e040e4fcacff37323cc978ec06f6b4
Kellett <halden.kellett1@DETNSW>
2016-11-16 14:24:49 +1100

    swappingtest files

--- Assets/Editor/BuildEditor.cs
+++ Assets/Editor/BuildEditor.cs
@@ -1,39 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 #if UNITY_EDITOR
 using UnityEditor;
-using System.Collections.Generic;
 
-public class BuildEditor : MonoBehaviour {
 
-    static string[] sceneNames = FindEnabledEditorScene();
+public class BuildEditor
+{
+    static string[] sceneNames = FindEnabledEditorScenes();
+
     [MenuItem("Build/Windows(x86)")]
     static void PerformWindowsx86Build()
     {
-        string targetDir = "Build/Windows(x86)"+PlayerSettings.productName+".exe";
-        GenericBuild(targetDir,BuildTarget.StandaloneWindows,BuildOptions.None);
+        string targetDir = "Build/Windows(x86)/" + PlayerSettings.productName + ".exe";
+        GenericBuild(targetDir, BuildTarget.StandaloneWindows, BuildOptions.None);
     }
+
     static void GenericBuild(string targetDir, BuildTarget buildTarget, BuildOptions buildOptions)
     {
+        // Switch the playform to target platform
         EditorUserBuildSettings.SwitchActiveBuildTarget(buildTarget);
-        string resu = BuildPipeline.BuildPlayer(sceneNames,targetDir,buildTarget, buildOptions);
-        if (resu.Length > 0)
+        // Perform the build
+        string result = BuildPipeline.BuildPlayer(sceneNames, targetDir, buildTarget, buildOptions);
+        // Check if there is an error
+        if (result.Length > 0)
         {
-            Debug.Log("Oi Mate there was a fuckn errer in the build "+resu);
-            System.Console.WriteLine("Oi Mate there was a fuckn errer in the build " + resu);
+            // Show error message
+            string error = "Build Failure: " + result;
+            // Print error to Unity
+            Debug.LogError(error);
+            // Print error to console
+            System.Console.WriteLine(error);
         }
     }
-    static string[] FindEnabledEditorScene()
+
+    static string[] FindEnabledEditorScenes()
     {
         List<string> editorScenes = new List<string>();
+
+        // Loop through all the scenes in the build settings
         foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
         {
-            editorScenes.Add(scene.path);
+            // Check if the current scene is enabled
+            if (scene.enabled)
+            {
+                // Add it to the list
+                editorScenes.Add(scene.path);
+            }
         }
-
 
         return editorScenes.ToArray();
     }
 }
-#endif
 
+#endif
